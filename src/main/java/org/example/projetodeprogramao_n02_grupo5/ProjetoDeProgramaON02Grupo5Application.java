dd6cdd388d79556b1a4b9e7fa56d795da8e5d13e
annamelmoura <annamelmoura3@gmail.com>
2025-10-20 19:20:17 -0300

    Add files via upload

---
+++ src/main/java/org/example/projetodeprogramao_n02_grupo5/ProjetoDeProgramaON02Grupo5Application.java
@@ -0,0 +1,13 @@
+package org.example.projetodeprogramao_n02_grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeProgramaON02Grupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeProgramaON02Grupo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/projetodeprogramao_n02_grupo5/ProjetoDeProgramaON02Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projetodeprogramao_n02_grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeProgramaON02Grupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
